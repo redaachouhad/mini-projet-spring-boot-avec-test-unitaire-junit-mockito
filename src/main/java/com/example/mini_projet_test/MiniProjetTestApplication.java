eb0f62cb1d7e23f07d7e3552ca4d2393b11718b9
redaachouhad <redachouhad@gmail.com>
2024-09-13 19:02:43 +0100

    changing readme.md

--- src/main/java/com/example/mini_projet_test/MiniProjetTestApplication.java
+++ src/main/java/com/example/mini_projet_test/MiniProjetTestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniProjetTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProjetTestApplication.class, args);
 	}
-
 }
--- src/test/java/com/example/mini_projet_test/MiniProjetTestApplicationTests.java
+++ src/test/java/com/example/mini_projet_test/MiniProjetTestApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.mini_projet_test;
 
+import com.example.mini_projet_test.controller.ProductController;
+import com.example.mini_projet_test.service.ProductService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiniProjetTestApplicationTests {
 
+	@Autowired
+	private ProductController productController;
+
+	@Autowired
+	private ProductService productService;
+
 	@Test
-	void contextLoads() {
+	void testApplicationContext() {
+		Assertions.assertNotNull(productController);
+		Assertions.assertNotNull(productService);
 	}
 
 }
